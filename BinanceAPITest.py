28b52d3ee0da14b745cb298422a2223004e96fb0
Bai <webai@paypal.com>
2019-06-27 11:08:49 +0800

    adding logging

--- BinanceAPITest.py
+++ BinanceAPITest.py
@@ -1,8 +1,18 @@
 from TradingPlatform.Binance import Binance
+import logging
+import sys
 
 def main():
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
     #Binance.buy('BTC', 'USDT', 30)
-    Binance.sell('BNB', 'USDT', 0.001)
+    Binance.sell('BTC', 'USDT', 0.001)
 
 if __name__ == '__main__':
     main()
